de7230bc85a75ce14b83ab94429358ee5d81d9e5
tom_glb <1278312643@qq.com>
2019-03-30 11:39:05 +0800

    多线程实现窗口取票

---
+++ src/elemOfopp/day11/TestWindow.java
@@ -0,0 +1,30 @@
+package elemOfopp.day11;
+//模拟火车站售票，开启三个床后售票，总片数100张
+public class TestWindow {
+	
+public static void main(String[] args) {
+	ThreadWindow1 w1=new ThreadWindow1();
+	ThreadWindow1 w2=new ThreadWindow1();
+	ThreadWindow1 w3=new ThreadWindow1();
+	w1.setName("窗口1");
+	w2.setName("窗口2");
+	w3.setName("窗口3");
+	w1.start();
+	w2.start();
+	w3.start();
+}
+}
+class ThreadWindow1 extends Thread{
+	static int count=100;
+
+	@Override
+	public void run() {
+		
+		
+		// TODO Auto-generated method stub
+		while(count>0) {
+			System.out.println(Thread.currentThread().getName()+" "+(100-count));
+			count--;
+		}
+	}
+}
